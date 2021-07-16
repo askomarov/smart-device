bf6874d6c1defa1e089833f96495744f5e4d315b
askomarov <askomarov13@mail.ru>
2021-07-16 19:13:36 +0300

    поправил js - убрали ошибки при возможных удалениях html блоков

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,14 @@
 /* eslint-env es6 */
 'use strict';
-
 // //////////////// аккордион
-const footer = document.querySelector('.footer');
-const accordionBtns = footer.querySelectorAll('.accordion__btn');
+// const body = document.querySelector('.page');
+const body = document.body;
 
-const addcordion = (mainBtns) => {
+const footer = document.querySelector('.footer');
+
+const accordionBtns = document.querySelectorAll('.accordion__btn');
+
+const accordion = (mainBtns) => {
   mainBtns.forEach((btn, i) => {
     btn.addEventListener('click', () => {
       mainBtns.forEach((btnj, j) => {
@@ -17,33 +20,30 @@ const addcordion = (mainBtns) => {
     });
   });
 };
-
 const openAccordionMenu = (mainBtns) => {
   mainBtns.forEach(btn => {
     btn.parentElement.classList.remove('accordion--open');
   });
 };
-
 const closeAccordionMenu = (mainBtns) => {
   mainBtns.forEach(btn => {
     btn.parentElement.classList.remove('accordion--open');
   });
 };
-
 const onMobileShowFooterAccordionMenu = (mainBtns) => {
   if (window.matchMedia('(max-width: 767.9px)').matches) {
     closeAccordionMenu(mainBtns);
-    addcordion(mainBtns);
+    accordion(mainBtns);
   } else {
     openAccordionMenu(mainBtns);
   }
 };
+const initAccordion = () => {
+  if (accordionBtns) {
+    onMobileShowFooterAccordionMenu(accordionBtns);
+  }
+};
 // ////////////////  КОНЕЦ аккордион
-
-// ////////////////  форма связи
-const feedbackForm = document.querySelector('.feedback__form');
-const phoneInput = feedbackForm.querySelector('#form-tel');
-const nameInput = feedbackForm.querySelector('#form-name');
 
 const helpOnInputTel = (input, event) => {
   if (!(event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'Backspace' || event.key === 'Tab')) {
@@ -80,7 +80,8 @@ const helpOnInputTel = (input, event) => {
 const ipnutTelHelperListener = (input) => {
   input.addEventListener('keydown', helpOnInputTel.bind(null, input));
 };
-
+// ////////////////  форма связи
+const feedbackForm = document.querySelector('.feedback__form');
 const showSuccessPopupMessage = (form) => {
   form.classList.add('form--sended');
   setTimeout(() => {
@@ -88,25 +89,32 @@ const showSuccessPopupMessage = (form) => {
   }, 750);
 };
 
-const onSubmitFeadbackForm = () => {
-  feedbackForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    localStorage.setItem('contactName', nameInput.value);
-    localStorage.setItem('contactTel', phoneInput.value);
-    feedbackForm.reset();
-    showSuccessPopupMessage(feedbackForm);
-  });
+const initFeedbackForm = () => {
+  if (feedbackForm) {
+    const phoneInput = feedbackForm.querySelector('#form-tel');
+    const nameInput = feedbackForm.querySelector('#form-name');
+
+    const onSubmitFeadbackForm = () => {
+      feedbackForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        localStorage.setItem('contactName', nameInput.value);
+        localStorage.setItem('contactTel', phoneInput.value);
+        feedbackForm.reset();
+        showSuccessPopupMessage(feedbackForm);
+      });
+    };
+
+    if (phoneInput) {
+      ipnutTelHelperListener(phoneInput);
+    }
+    onSubmitFeadbackForm();
+  }
 };
 // ////////////////  КОНЕЦ форма связи
 
 // /////////////// модальное окно
-const body = document.querySelector('.page');
+
 const header = body.querySelector('.header');
-const modal = document.querySelector('.modal');
-const modalForm = modal.querySelector('.modal__form');
-const buttonOpenModal = document.querySelector('.modal-btn');
-const inputTelModalForm = modalForm.querySelector('#modal-form-tel');
-const inputNameModalForm = modalForm.querySelector('#modal-form-name');
 
 const getScrollWidth = () => {
   let div = document.createElement('div');
@@ -136,104 +144,117 @@ const isEscEvent = (evt) => {
   return evt.key === ('Escape' || 'Esc');
 };
 
-let bindHelpOnInputTel = helpOnInputTel.bind(null, inputTelModalForm);
+const modal = document.querySelector('.modal');
+const buttonOpenModal = document.querySelector('.modal-btn');
 
-const closeModal = () => {
-  modal.setAttribute('aria-hidden', 'true');
-  unlockBody();
-  modalForm.reset();
-  document.removeEventListener('keydown', onEscKeydownCloseModal);
-  document.removeEventListener('click', onClickAwayCloseModal);
-  inputTelModalForm.removeEventListener('keydown', bindHelpOnInputTel);
-  header.setAttribute('aria-hidden', 'false');
-  footer.setAttribute('aria-hidden', 'false');
-};
+const initModalForm = () => {
+  if (buttonOpenModal && modal) {
+    const modalForm = modal.querySelector('.modal__form');
+    const inputTelModalForm = modalForm.querySelector('#modal-form-tel');
+    const inputNameModalForm = modalForm.querySelector('#modal-form-name');
+    let bindHelpOnInputTel = helpOnInputTel.bind(null, inputTelModalForm);
 
-const onBtnCloseModal = (btnClose) => {
-  if (modal) {
-    btnClose.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      closeModal();
-    }, { once: true });
+    const closeModal = () => {
+      modal.setAttribute('aria-hidden', 'true');
+      unlockBody();
+      modalForm.reset();
+      document.removeEventListener('keydown', onEscKeydownCloseModal);
+      document.removeEventListener('click', onClickAwayCloseModal);
+      inputTelModalForm.removeEventListener('keydown', bindHelpOnInputTel);
+
+      if (header) {
+        header.setAttribute('aria-hidden', 'false');
+      }
+      if (footer) {
+        footer.setAttribute('aria-hidden', 'false');
+      }
+    };
+
+    const onBtnCloseModal = (btnClose) => {
+      btnClose.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        closeModal();
+      }, {once: true});
+    };
+
+    const onEscKeydownCloseModal = (evt) => {
+      if (isEscEvent(evt)) {
+        evt.preventDefault();
+        closeModal();
+      }
+    };
+
+    const onClickAwayCloseModal = (evt) => {
+      if (!evt.target.closest('.modal__wrapper')) {
+        closeModal();
+      }
+    };
+
+    const addListenerOnOpenModal = () => {
+      setTimeout(() => {
+        document.addEventListener('click', onClickAwayCloseModal);
+        document.addEventListener('keydown', onEscKeydownCloseModal);
+      }, 200);
+    };
+
+    const openModal = () => {
+      modal.setAttribute('aria-hidden', 'false');
+      inputTelModalForm.focus();
+      let bodyLockPadding = getScrollWidth();
+      lockBody(bodyLockPadding);
+
+      const btnClose = modalForm.querySelector('.form__btn-close');
+      onBtnCloseModal(btnClose, modal);
+
+      addListenerOnOpenModal();
+      inputTelModalForm.addEventListener('keydown', bindHelpOnInputTel);
+
+      if (header) {
+        header.setAttribute('aria-hidden', 'true');
+      }
+      if (footer) {
+        footer.setAttribute('aria-hidden', 'true');
+      }
+    };
+
+    const onSuccessSubmit = () => {
+      showSuccessPopupMessage(modalForm);
+      setTimeout(() => {
+        closeModal();
+      }, 750);
+    };
+
+    const onSubmitModalFormSendData = () => {
+      modalForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        localStorage.setItem('modal-tel', inputTelModalForm.value);
+        localStorage.setItem('modal-mail', inputNameModalForm.value);
+        onSuccessSubmit();
+      }, {once: true});
+    };
+
+    const onBtnShowModal = (btn) => {
+      btn.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        openModal();
+        onSubmitModalFormSendData();
+      });
+    };
+
+    onBtnShowModal(buttonOpenModal);
   }
 };
 
-const onEscKeydownCloseModal = (evt) => {
-  if (isEscEvent(evt)) {
-    evt.preventDefault();
-    closeModal();
-  }
-};
-
-const onClickAwayCloseModal = (evt) => {
-  if (!evt.target.closest('.modal__wrapper')) {
-    closeModal();
-  }
-};
-
-const addListenerOnOpenModal = () => {
-  setTimeout(() => {
-    document.addEventListener('click', onClickAwayCloseModal);
-    document.addEventListener('keydown', onEscKeydownCloseModal);
-  }, 200);
-};
-
-const openModal = () => {
-  modal.setAttribute('aria-hidden', 'false');
-  inputTelModalForm.focus();
-  let bodyLockPadding = getScrollWidth();
-  lockBody(bodyLockPadding);
-
-  const btnClose = modalForm.querySelector('.form__btn-close');
-  onBtnCloseModal(btnClose, modal);
-
-  addListenerOnOpenModal();
-  inputTelModalForm.addEventListener('keydown', bindHelpOnInputTel);
-
-  header.setAttribute('aria-hidden', 'true');
-  footer.setAttribute('aria-hidden', 'true');
-};
-
-const onSuccessSubmit = () => {
-  showSuccessPopupMessage(modalForm);
-  setTimeout(() => {
-    closeModal();
-  }, 750);
-};
-
-const onSubmitModalFormSendData = () => {
-  modalForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    localStorage.setItem('modal-tel', inputTelModalForm.value);
-    localStorage.setItem('modal-mail', inputNameModalForm.value);
-    onSuccessSubmit();
-  }, { once: true });
-};
-
-const onBtnShowModal = (btn) => {
-  btn.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    openModal();
-    onSubmitModalFormSendData();
-  });
-};
 // /////////////// КОНЕЦ модальное окно
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.classList.remove('page--no-js');
   // аккордион в подвале на мобильном
-  if (accordionBtns) {
-    onMobileShowFooterAccordionMenu(accordionBtns);
-  }
+  initAccordion();
+
   // форма обратной свзяи
-  if (phoneInput) {
-    ipnutTelHelperListener(phoneInput);
-  }
-  if (feedbackForm) {
-    onSubmitFeadbackForm();
-  }
+  initFeedbackForm();
+
   // модальное окно
-  if (buttonOpenModal && modalForm) {
-    onBtnShowModal(buttonOpenModal);
-  }
+  initModalForm();
 });
